d9cea1a46a05a86983b58ddb5a35cf2ba9d47ecf
LienaPuey <lienapuey@gmail.com>
2023-12-03 17:09:26 +0100

    article-list

---
+++ PEC4_Ej_Prac/ecommerce/src/app/article-list/article-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Article } from '../article';
+
+@Component({
+  selector: 'app-article-list',
+  templateUrl: './article-list.component.html',
+  styleUrls: ['./article-list.component.css']
+})
+export class ArticleListComponent {
+  
+}
