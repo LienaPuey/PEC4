74fd0bc31ad040f8d5da636e062e0f8e2dfb2780
LienaPuey <lienapuey@gmail.com>
2023-12-03 14:25:04 +0100

    botones

--- PEC4_Ej_Prac/ecommerce/src/app/article-item/article-item.component.ts
+++ PEC4_Ej_Prac/ecommerce/src/app/article-item/article-item.component.ts
@@ -7,11 +7,28 @@ import { Article } from '../article';
   styleUrls: ['./article-item.component.css']
 })
 export class ArticleItemComponent {
+  quantity:number;
+
+  constructor(){
+    this.quantity = 0;
+  }
   product: Article = {
     name: "Camiseta de mujer",
     imageUrl:'../../assets/camiseta_mujer.jpeg',
     price: 12.50,
     isOnSale: true,
     quantityInCart: 12
+  }
+
+  addQuantity(){
+    if(this.product.isOnSale){
+      this.quantity++;
+    }
+  }
+
+  removeQuantity(){
+    if(this.product.isOnSale && this.quantity > 0){
+      this.quantity--;
+    }
   }
 }
