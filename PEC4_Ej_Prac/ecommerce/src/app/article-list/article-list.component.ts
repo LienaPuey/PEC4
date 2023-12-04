40afb803c7bb9bc64e866c9352521074a8bb7bf1
unknown <lienapuey@gmail.com>
2023-12-04 20:55:36 +0100

    comienzo

--- PEC4_Ej_Prac/ecommerce/src/app/article-list/article-list.component.ts
+++ PEC4_Ej_Prac/ecommerce/src/app/article-list/article-list.component.ts
@@ -3,9 +3,15 @@ import { Article } from '../article';
 
 @Component({
   selector: 'app-article-list',
-  templateUrl: './article-list.component.html',
+  template: `<div ngFor*="let article of articles" >
+    <app-article-item ></app-article-item>
+  </div>`,
   styleUrls: ['./article-list.component.css']
 })
 export class ArticleListComponent {
-  
+  articles:Article[]=[
+    {id: 1, name: "Camiseta de mujer",  imageUrl:'../../assets/camiseta_mujer.jpeg', price: 12.50, isOnSale: true, quantityInCart: 12},
+    {id: 2, name: "Pantalones vaqueros",  imageUrl:'../../assets/vaqueros.jpg', price: 25, isOnSale: true, quantityInCart: 10},
+    {id: 3, name: "Zapatillas",  imageUrl:'../../assets/zapatillas.PNG', price: 35, isOnSale: true, quantityInCart: 5}
+  ]
 }
